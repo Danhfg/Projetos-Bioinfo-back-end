54e68073a2a305d130d3e6409760b92052b1a8a6
Danhfg <danielhenriquefg@gmail.com>
2020-10-22 14:09:25 -0300

    create spring boot project with maven

---
+++ src/main/java/br/ufrn/imd/bioinfo/projetos/Main.java
@@ -0,0 +1,15 @@
+package br.ufrn.imd.bioinfo.projetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+        // TODO Auto-generated method stub
+
+    }
+
+}
