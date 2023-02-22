e26f69448219dc849938da9e3a3b0f80ca0d435f
Daniel Henrique <danielhenriquefg@gmail.com>
2023-02-22 13:42:26 -0300

    Update SecurityConfig.java

--- src/main/java/br/ufrn/imd/bioinfo/projetos/config/SecurityConfig.java
+++ src/main/java/br/ufrn/imd/bioinfo/projetos/config/SecurityConfig.java
@@ -59,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			 .antMatchers("/daniel_backend/api/validation/**").permitAll()
 			 .antMatchers("/daniel_backend/api/reset/**").permitAll()
 			 .antMatchers("/daniel_backend/api/user").permitAll()
+			 .antMatchers("/daniel_backend/api/**").permitAll()
 			 //.antMatchers("/daniel_backend/**").permitAll()
 			 .antMatchers("/api/sign-in/**").permitAll()
 			 .antMatchers("/api/sign-up/**").permitAll()
