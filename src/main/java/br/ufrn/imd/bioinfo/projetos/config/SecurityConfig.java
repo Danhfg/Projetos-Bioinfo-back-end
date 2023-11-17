5df38f105b57207e127a0e13d9a8a52663d762d4
Daniel Henrique Ferreira Gomes <danielhenriquefg@gmail.com>
2023-11-17 09:35:30 -0300

    fix cors

--- src/main/java/br/ufrn/imd/bioinfo/projetos/config/SecurityConfig.java
+++ src/main/java/br/ufrn/imd/bioinfo/projetos/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		 http.csrf().disable();
 		 
-		 http.cors(cors -> cors.disable());
+		 //http.cors(cors -> cors.disable());
 
 		 http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
